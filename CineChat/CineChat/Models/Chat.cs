179dfc497fbcf22f1934d7c40bb7c7f77276de62
kjohn47 <joaomorgado0@gmail.com>
2015-11-10 22:14:22 +0000

    chat crud - creation with category and list chat with category

--- CineChat/CineChat/Models/Chat.cs
+++ CineChat/CineChat/Models/Chat.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,11 +8,14 @@ namespace CineChat.Models
 {
     public class Chat
     {
-
+        [Required]
         public int ID { get; set; }
 
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
         public string title { get; set; }
 
+        [DataType(DataType.Password)]
         public string password { get; set; }
 
         public virtual ApplicationUser admin { get; set; }
