84fc6eab44e4ecddf88e7291c4e22e8e7c095590
kjohn47 <joaomorgado0@gmail.com>
2015-11-11 23:01:34 +0000

    Chat system - crud and post system working with DB

---
+++ CineChat/CineChat/Controllers/PostsController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CineChat.DAL;
+using CineChat.Models;
+using Microsoft.AspNet.Identity;
+
+namespace CineChat.Controllers
+{
+    public class PostsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Posts
+        [Authorize]
+        public ActionResult Index()
+        {
+            //return View(db.post.ToList());
+            return RedirectToAction("Index", "Chats");
+        }
+
+        // GET: Posts/Details/5
+        [Authorize]
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Post post = db.post.Find(id);
+            if (post == null)
+            {
+                return HttpNotFound();
+            }
+            return View(post);
+        }
+
+        // GET: Posts/Create
+        [Authorize]
+        public ActionResult Create(int? id)
+        {
+            if(id == null)
+            {
+                return RedirectToAction("Index", "Chats");
+            }
+            ViewBag.chatID = id;
+            return View();
+        }
+
+        // POST: Posts/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,message,time")] Post post, int? chatid)
+        {
+            if(chatid == null)
+            {
+                return RedirectToAction("Index", "Chats");
+            }
+            if (ModelState.IsValid)
+            {
+                string currentUserId = User.Identity.GetUserId();
+                var currentUser = db.Users.FirstOrDefault(x => x.Id == currentUserId);
+                var currentchat = db.chat.FirstOrDefault(y => y.ID == chatid);
+                post.time = DateTime.Now;
+                db.post.Add(post);
+                currentUser.posts.Add(post);
+                currentchat.posts.Add(post);
+                db.SaveChanges();
+                return RedirectToAction("Details/" + chatid, "Chats");
+            }
+            ViewBag.chatID = chatid;
+            return View(post);
+        }
+
+        // GET: Posts/Edit/5
+        [Authorize]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Post post = db.post.Find(id);
+            if (post == null)
+            {
+                return HttpNotFound();
+            }
+            return View(post);
+        }
+
+        // POST: Posts/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,message,time")] Post post)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(post).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(post);
+        }
+
+        // GET: Posts/Delete/5
+        [Authorize]
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Post post = db.post.Find(id);
+            if (post == null)
+            {
+                return HttpNotFound();
+            }
+            return View(post);
+        }
+
+        // POST: Posts/Delete/5
+        [Authorize]
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Post post = db.post.Find(id);
+            db.post.Remove(post);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
