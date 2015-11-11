4223ad95e61f917cedf15027c61ef2c52244b4be
ajrgoncalves <ajrg@ua.pt>
2015-11-11 22:50:03 +0000

    RestIncompleto

---
+++ CineChat/CineChat/Controllers/FilmesIMDBController.cs
@@ -0,0 +1,44 @@
+﻿using CineChat.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web;
+
+namespace CineChat.Controllers
+{
+    public class FilmesIMDBController : ApiController
+    {
+
+        static void FilmesIMDB()
+        {
+            RunAsync().Wait();
+        }
+
+        static async Task RunAsync()
+        {
+            using (var client = new HttpClient())
+            {
+                // New code:
+                client.BaseAddress = new Uri("http://www.imdb.com/movies-in-theaters/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage response = await client.GetAsync("api/products/1");
+                if (response.IsSuccessStatusCode)
+                {
+                    FilmesIMDB filmes = await response.Content.ReadAsAsync<FilmesIMDB>();
+                    Console.WriteLine("{0}\t${1}\t{2}", filmes.Name, filmes.Actors, filmes.Category);
+                }
+
+
+            }
+
+        }
+    }
+}
---
+++ CineChat/CineChat/Models/FilmesIMDB.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CineChat.Models
+{
+    public class FilmesIMDB
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public int Year { get; set; }
+        public DateTime Released { get; set; }
+        public string RunTime { get; set; }
+        public string Genre { get; set; }
+        public string Director { get; set; }
+        public string Writer { get; set; }
+        public string Actors { get; set; }
+        public string Plot { get; set; }
+        public string Language { get; set; }
+        public string Awards { get; set; }
+        public string Poster { get; set; }
+        public int Metascore { get; set; }
+        public float RatingIMDB { get; set; }
+        public string ImdbID { get; set; }
+        public int IMDBVotes { get; set; }
+        public string Type { get; set; }
+        public bool Response { get; set; }
+
+
+
+
+
+    }
+}
