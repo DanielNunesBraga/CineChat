68a6d038895ac75d8522fa2989d037792e2265d0
kjohn47 <joaomorgado0@gmail.com>
2015-11-11 21:20:38 +0000

    viewmodel 2nd try

---
+++ CineChat/CineChat/ViewModels/AssignedCategoryData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CineChat.DAL;
+
+namespace CineChat.ViewModels
+{
+    public class AssignedCategoryData
+    {
+        public int CategoryID { get; set; }
+        public string Description { get; set; }
+        public bool Assigned { get; set; }
+    }
+
+}
